247b4c61c82095ae5784406badf8459fe26f269e
kieran <kieran@harkin.me>
2025-02-17 16:02:09 +0000

    feat: add contact for account page

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -22,6 +22,13 @@ export default function AccountPage() {
   return (
     <>
       <h3>My Resources</h3>
+      <div className="rounded-xl bg-red-400 text-black p-3 font-bold">
+        Something doesnt look right? <br />
+        Please contact support on: {" "}
+        <a href={`mailto:sales@lnvps.net?subject=[${login?.publicKey}]%20Account%20Query`} className="underline">
+          sales@lnvps.net
+        </a>
+      </div>
       <div className="flex flex-col gap-2">
         {vms.map((a) => (
           <VpsInstanceRow key={a.id} vm={a} onReload={loadVms} />

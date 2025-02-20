31f0a3c925b77b899ce39ddd4a4dab57f9da835f
kieran <kieran@harkin.me>
2025-02-20 21:55:10 +0000

    feat: add npub to account page

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { VmInstance } from "../api";
 import useLogin from "../hooks/login";
 import VpsInstanceRow from "../components/vps-instance";
+import { hexToBech32 } from "@snort/shared";
 
 export default function AccountPage() {
   const login = useLogin();
@@ -19,13 +20,18 @@ export default function AccountPage() {
     return () => clearInterval(t);
   }, [login]);
 
+  const npub = hexToBech32("npub", login?.publicKey);
   return (
     <>
+      <div className="flex flex-col gap-2">
+        Your Public Key:
+        <pre className="bg-neutral-800 rounded-md px-3 py-2 select-all text-sm">{npub}</pre>
+      </div>
       <h3>My Resources</h3>
       <div className="rounded-xl bg-red-400 text-black p-3 font-bold">
         Something doesnt look right? <br />
         Please contact support on: {" "}
-        <a href={`mailto:sales@lnvps.net?subject=[${login?.publicKey}]%20Account%20Query`} className="underline">
+        <a href={`mailto:sales@lnvps.net?subject=[${npub}]%20Account%20Query`} className="underline">
           sales@lnvps.net
         </a>
       </div>
